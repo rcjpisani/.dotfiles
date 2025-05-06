5eeba8b281bd6f0c626ba1fbcca7b9ba03650d1e
Robert Pisani <rob@pisani.ca>
2025-05-05 22:17:45 -0400

    Replace Neovim configs with kickstart.nvim

--- nvim/.config/nvim/after/plugin/splitjoin.lua
+++
@@ -1,4 +0,0 @@
-
--- Splitjoin keymaps
-vim.keymap.set("n", "ss", vim.cmd.SplitjoinSplit, { desc = "[S]plitjoin [S]plit" })
-vim.keymap.set("n", "sj", vim.cmd.SplitjoinJoin, { desc = "[S]plitjoin [J]oin" })
--- nvim/.config/nvim/after/plugin/undotree.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, {
-  desc = 'Show [U]ndo tree'
-})
